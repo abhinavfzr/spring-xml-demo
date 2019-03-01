776ec0cccb32e83bd142eff31d44c41c864c058c
abhinavfzr <abhi.king11@gmail.com>
2019-03-01 13:15:32 +0530

    second commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,20 +1,18 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class Main
 {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie= (Movie) context.getBean("movie");
-        System.out.println(movie+" by application context ");
-        XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie mybean= (Movie)beanFactory.getBean("movie");
-        System.out.println(mybean+" by xml bean factory");
+        Movie movie1= (Movie) context.getBean("movie1");
+        System.out.println(movie+" movie bean 1 by application context ");
+        System.out.println(movie1+" movie bean 2 by application context ");
+        System.out.println(movie==movie1);
 
     }
 }
