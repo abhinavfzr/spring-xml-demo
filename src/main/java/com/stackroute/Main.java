7abd919cad0acd40356513f58aa3c883b0475fbd
abhinavfzr <abhi.king11@gmail.com>
2019-03-01 16:04:11 +0530

    fifth commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,18 +1,16 @@
 package com.stackroute;
 
-import com.stackroute.domain.Movie;
+import com.stackroute.demo.BeanLifeCycleDemoBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class Main
 {
     public static void main(String[] args) {
-        Movie movie= new Movie();
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        movie.setApplicationContext(context);
-        movie.setBeanName("movie");
-        System.out.println(movie.getBeanName()+" movie bean 1 by application context ");
-      
+        ((ClassPathXmlApplicationContext) context).registerShutdownHook();
+        BeanLifeCycleDemoBean beanLifeCycleDemoBean= (BeanLifeCycleDemoBean)context.getBean("BeanLifeCycleDemoBean");
 
     }
 }
---
+++ src/main/java/com/stackroute/demo/BeanLifeCycleDemoBean.java
@@ -0,0 +1,26 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifeCycleDemoBean implements InitializingBean, DisposableBean
+{
+
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("after properties set");
+
+    }
+    public  void  customInit()
+    {
+        System.out.println("start custom bean");
+    }
+    public  void  customDestroy()
+    {
+        System.out.println("Destroy custom bean");
+
+    }
+}
