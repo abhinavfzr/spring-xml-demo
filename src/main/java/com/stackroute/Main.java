cb2f7c39b3769a51484362509c2eded080a3b89a
abhinav jaiswal <abhi.king11@gmail.com>
2019-03-07 11:05:39 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -4,6 +4,20 @@ import com.stackroute.demo.BeanLifeCycleDemoBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+// From the aware-interface branch of spring-xml-demo repo create a bean-lifecycle
+// branch.
+// Add a BeanLifecycleDemoBean class in com.stackroute.demo that implements
+// InitializingBean and DisposableBean.
+// Override the required methods to print out messages.
+// Define BeanLifecycleDemoBean as a bean in beans.xml.
+// Run the application and observe the result. 
+// Add two methods customInit() and customDestroy() to the BeanLifecycleDemoBean
+// class and print out custom messages.
+// In the BeanLifecycleDemoBean bean definition, in beans.xml, set the customInit()
+// and customDestroy() methods to be called.
+// Run the application.
+// Push the code to bean-lifecycle branch. 
+
 
 public class Main
 {
