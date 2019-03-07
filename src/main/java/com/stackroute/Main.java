05558b37bf7bb25948c5d3d592ac975b878c07d2
abhinav jaiswal <abhi.king11@gmail.com>
2019-03-07 11:04:00 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -4,22 +4,16 @@ import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-// From the master branch of spring-xml-demo repo create a constructor-injection
-// branch.
-// Add constructor to the Actor class to initialize with name and gender, and age
-// Create three beans of type Actor in the bean definition file.
-// Use constructor-based injection in the bean definition file (beans.xml) to inject property values in
-// each of the three beans via name, index, and type respectively.
-// For the Movie bean, use constructor based object injection in the bean definition file
-// (beans.xml) to inject an Actor bean.
-// In the Main class, look up Movie bean using ApplicationContext and print out Author
-// information.
-// Use the same ApplicationContext to again look up the same Movie bean.
-// Print out the equality result of the two Movie beans.
-// System.out.println(beanA==beanB);
-// Change the scope of the Movie bean in beans.xml to prototype and run the application again.
-// Note the output.
-// Replace id of the Movie bean with name having two values, like this: 
+// From the constructor-injection branch of spring-xml-demo repo create a
+// autowire-xml branch.
+// For the Movie bean, delete the constructor based object injection in the bean definition file
+// (beans.xml) that injects an Actor bean.
+// Use autowire byName in the Movie bean to inject an Actor bean.
+// Run the application.
+// Create another Movie bean and try autowire byType.
+// Run the application and note the exception thrown.
+// Fix the Movie bean by removing autowire byType and using constructor injection instead.
+// Push the code to autowire-xml branch. : 
 
 public class Main
 {
